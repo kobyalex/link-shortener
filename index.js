00e3fc7602aec4e86f9c64802ae08b75c64e6f2e
kobyalex <kobyalex@users.noreply.github.com>
2022-01-26 14:55:29 +0100

    Second approach

--- index.js
+++ index.js
@@ -1,110 +1,103 @@
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
+});
+
 const html404 = `<!DOCTYPE html>
-<body>
-  <h1>404 Not Found.</h1>
-  <p>The url you visit is not found.</p>
-</body>`
+  <body>
+    <h1>404 Not Found.</h1>
+    <p>The url you visit is not found.</p>
+  </body>`;
 
+/**
+ * Respond with hello worker text
+ * @param {Request} request
+ */
 
+async function returnHTML() {
+  const html = await fetch(
+    "https://cdn.jsdelivr.net/gh/kobyalex/link-shortener@main/index2.html"
+  );
+  /****customized index.html at main branch, easier to edit it****/
+
+  return new Response(await html.text(), {
+    headers: {
+      "content-type": "text/html;charset=UTF-8",
+    },
+  });
+}
 async function randomString(len) {
-    len = len || 6;
-    let $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';    
-    /****Removed confusing letters an numbers, oOLl,9gq,Vv,Uu,I1****/
-    let maxPos = $chars.length;
-    let result = '';
-    for (i = 0; i < len; i++) {
-        result += $chars.charAt(Math.floor(Math.random() * maxPos));
-    }
-    return result;
+  len = len || 6;
+  let $chars = "ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678";
+  let maxPos = $chars.length;
+  let result = "";
+  for (i = 0; i < len; i++) {
+    result += $chars.charAt(Math.floor(Math.random() * maxPos));
+  }
+  return result;
 }
-async function checkURL(URL){
-    let str=URL;
-    let Expression=/http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/;
-    let objExp=new RegExp(Expression);
-    if(objExp.test(str)==true){
-      if (str[0] == 'h')
-        return true;
-      else
-        return false;
-    }else{
-        return false;
-    }
-} 
-async function save_url(URL){
-    let random_key=await randomString()
-    let is_exist=await LINKS.get(random_key)
-    console.log(is_exist)
-    if (is_exist == null)
-        return await LINKS.put(random_key, URL),random_key
-    else
-        save_url(URL)
+
+async function checkURL(URL) {
+  let str = URL;
+  let Expression = /http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/;
+  let objExp = new RegExp(Expression);
+  if (objExp.test(str) == true) {
+    if (str[0] == "h") return true;
+    else return false;
+  } else {
+    return false;
+  }
 }
+
 async function handleRequest(request) {
-    let header = {
-        "content-type": "text/html;charset=UTF-8",
-        "Access-Control-Allow-Origin":"*",
-        "Access-Control-Allow-Methods": "POST",
-    }
-  console.log(request)
   if (request.method === "POST") {
-    let req=await request.json()
-    console.log(req["url"])
-    if(!await checkURL(req["url"])){
-    return new Response(`{"status":500,"key":": Error: Url illegal."}`, {
-      headers: header,
-    })}
-    let stat,random_key=await save_url(req["url"])
-    console.log(stat)
-    if (typeof(stat) == "undefined"){
-      return new Response(`{"status":200,"key":"/`+random_key+`"}`, {
-      headers: header,
-    })
+    const urls = await request.json();
+    let urlb = JSON.parse(JSON.stringify(urls));
+    longURL = urlb.longURL;
+    shortURL = urlb.shortURL;
+
+    if (shortURL == "") {
+      shortURL = await randomString();
     }
-    // 
-    else{
-      return new Response(`{"status":200,"key":": Error:Reach the KV write limitation."}`, {
-      headers: header,
-    })}
-  }else if(request.method === "OPTIONS"){  
-      return new Response(``, {
-      headers: header,
-    })
 
+    if (!(await checkURL(longURL))) {
+      return new Response(" Illegal URL Detected. Please Enter a Valid URL ");
+    }
+
+    let is_exist = await URL_SPACE.get(shortURL);
+
+    if (is_exist == null) {
+      await URL_SPACE.put(shortURL, longURL);
+      return new Response("Your short URL: http://localhost:8787/" + shortURL);
+    } else {
+      return new Response(
+        " Duplicate Alias Detected. Enter a different Alias. "
+      );
+    }
   }
 
-  const requestURL = new URL(request.url)
-  const path = requestURL.pathname.split("/")[1]
-  console.log(path)
-  if(!path){
+  const htmlBody = await returnHTML();
 
-    const html= await fetch("https://cdn.jsdelivr.net/gh/51sec/Url-Shorten-By-CF-Worker@main/index.html")
-    /****customized index.html at main branch, easier to edit it****/
-    
-    return new Response(await html.text(), {
-    headers: {
-      "content-type": "text/html;charset=UTF-8",
-    },
-  })
-  }
-  const value = await LINKS.get(path)
-  console.log(value)
-  
+  if (request.method === "GET") {
+    let shortCode = request.url.replace(/https:\/\/.+?\//g, "");
+    shortCode = shortCode.replace(/http:\/\/.+?\//g, "");
 
-  const location = value
-  if (location) {
-    return Response.redirect(location, 302)
-    
+    if (shortCode !== "") {
+      let redirectTo = await URL_SPACE.get(shortCode);
+
+      if (redirectTo != null) {
+        return Response.redirect(redirectTo, 301);
+      } else {
+        return new Response(html404, {
+          headers: {
+            "content-type": "text/html;charset=UTF-8",
+          },
+          status: 404,
+        });
+      }
+    } else {
+      return new Response(htmlBody, {
+        headers: { "content-type": "text/html" },
+      });
+    }
   }
-  // If request not in kv, return 404
-  return new Response(html404, {
-    headers: {
-      "content-type": "text/html;charset=UTF-8",
-    },
-    status: 404
-  })
 }
-
-
-
-addEventListener("fetch", async event => {
-  event.respondWith(handleRequest(event.request))
-})
